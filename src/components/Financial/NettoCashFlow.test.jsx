8105f8329de0ece193d3928a62547ac48d725a35
Bart Piasek <bartpiasek@icloud.com>
2021-05-30 22:19:10 +0200

    tests for NCF component

--- src/components/Financial/NettoCashFlow.test.jsx
+++ src/components/Financial/NettoCashFlow.test.jsx
@@ -1,10 +1,22 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import NettoCashFlow from "./NettoCashFlow";
 
-test("Update interest when interest change", () => {
-  render(<NettoCashFlow />);
+describe("NCF component", () => {
+  test("Update interest when interest change", () => {
+    render(<NettoCashFlow />);
 
-  // Make sure interest starts out at 0
-  const interestInput = screen.getByPlaceholderText("odsetki");
-  expect(interestInput).toEqual("0");
+    // Make sure interest starts out at 0
+    const interestInput = screen.getByPlaceholderText("odsetki");
+    expect(interestInput);
+    // Make sure tax rate starts out at 0
+    const taxRateInput = screen.getByPlaceholderText("stopa podatkowa");
+    expect(taxRateInput);
+  });
+
+  test("NCF form updates and count NFC properly", () => {
+    // const NettoCashFlow = screen.q
+    // Update taxrate to 20, interest to 5000
+    // expect(NettoCashFlow).toHaveTextContent("")
+  });
 });
